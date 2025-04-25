d8934f88f7a385403c72f76203b23dcd053ee5fc
870421 <870421@unizar.es>
2025-04-25 11:52:15 +0200

    mejora consulta3

---
+++ sql/mejora3.sql
@@ -0,0 +1,35 @@
+CREATE MATERIALIZED VIEW MV_AEROPUERTOS_AVIONES_JOVENES
+REFRESH COMPLETE ON DEMAND
+AS
+WITH AvionesEnAeropuertos AS (
+    SELECT v.aeropuertoSalida AS codigo_aeropuerto, v.avion, a.agnoFabricacion
+    FROM VUELO v
+    JOIN AVION a ON v.avion = a.matricula
+    UNION
+    SELECT v.aeropuertoLlegada AS codigo_aeropuerto, v.avion, a.agnoFabricacion
+    FROM VUELO v
+    JOIN AVION a ON v.avion = a.matricula
+),
+EdadPromedioPorAeropuerto AS (
+    SELECT codigo_aeropuerto, AVG(2024 - agnoFabricacion) AS edad_promedio
+    FROM AvionesEnAeropuertos
+    GROUP BY codigo_aeropuerto
+),
+AeropuertoConAvionesJovenes AS (
+    SELECT codigo_aeropuerto, edad_promedio
+    FROM EdadPromedioPorAeropuerto
+    WHERE edad_promedio = (SELECT MIN(edad_promedio) FROM EdadPromedioPorAeropuerto)
+)
+SELECT a.codigoIATA, a.nombre, aaj.edad_promedio AS media_edad_aviones
+FROM AeropuertoConAvionesJovenes aaj
+JOIN AEROPUERTO a ON aaj.codigo_aeropuerto = a.codigoIATA;
+
+/* Para ejecutar los comando para ver el resultado de la vista materializada, 
+   primero se debe crear la vista materializada y luego ejecutar el siguiente código
+   
+   EXPLAIN PLAN FOR 
+   SELECT * FROM MV_AEROPUERTOS_AVIONES_JOVENES;
+
+   SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY());
+   
+   */
