6ebc957fb86386cc03d3899c60532a3ab39f8b5a
870421 <870421@unizar.es>
2025-04-25 17:50:07 +0200

    cambios script mario

--- poblarTablasMario.sh
+++ poblarTablasMario.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 # Script para poblar las tablas de la base de datos
-sqlldr a872342@barret.danae04.unizar.es control=datosAeropuerto.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosAeropuerto.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosCompagnia.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosCompagnia.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosModelo.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosModelo.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosAvion.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosAvion.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosVuelo.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosVuelo.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosIncidencia.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosIncidencia.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosRetraso.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosRetraso.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosDesvio.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosDesvio.ctl <<EOF
 monteagudo9
 EOF
-sqlldr a872342@barret.danae04.unizar.es control=datosCancelacion.ctl <<EOF
+sqlldr a870421@barret.danae04.unizar.es control=datosCancelacion.ctl <<EOF
 monteagudo9
 EOF
