45c221dd467b3730d04a0fd24c4dafe7293f9620
carlosalejoss <872342@unizar.es>
2025-04-30 13:38:30 +0200

    Refactor MV_AEROPUERTOS_AVIONES_JOVENES view by removing unnecessary CTE and simplifying the query

--- sql/mejora3.sql
+++ sql/mejora3.sql
@@ -1,18 +1,18 @@
 CREATE MATERIALIZED VIEW MV_AEROPUERTOS_AVIONES_JOVENES
 REFRESH COMPLETE ON DEMAND
 AS
-WITH AvionesEnAeropuertos AS (
-    SELECT v.aeropuertoSalida AS codigo_aeropuerto, v.avion, a.agnoFabricacion
-    FROM VUELO v
-    JOIN AVION a ON v.avion = a.matricula
-    UNION
-    SELECT v.aeropuertoLlegada AS codigo_aeropuerto, v.avion, a.agnoFabricacion
-    FROM VUELO v
-    JOIN AVION a ON v.avion = a.matricula
-),
-EdadPromedioPorAeropuerto AS (
+SELECT v.aeropuertoSalida AS codigo_aeropuerto, v.avion, a.agnoFabricacion
+FROM VUELO v
+JOIN AVION a ON v.avion = a.matricula
+UNION
+SELECT v.aeropuertoLlegada AS codigo_aeropuerto, v.avion, a.agnoFabricacion
+FROM VUELO v
+JOIN AVION a ON v.avion = a.matricula;
+
+/* 
+WITH EdadPromedioPorAeropuerto AS (
     SELECT codigo_aeropuerto, AVG(2025 - agnoFabricacion) AS edad_promedio
-    FROM AvionesEnAeropuertos
+    FROM MV_AEROPUERTOS_AVIONES_JOVENES
     GROUP BY codigo_aeropuerto
 ),
 AeropuertoConAvionesJovenes AS (
@@ -23,13 +23,4 @@ AeropuertoConAvionesJovenes AS (
 SELECT a.IATA, a.nombre, aaj.edad_promedio AS media_edad_aviones
 FROM AeropuertoConAvionesJovenes aaj
 JOIN AEROPUERTO a ON aaj.codigo_aeropuerto = a.IATA;
-
-/* Para ejecutar los comando para ver el resultado de la vista materializada, 
-   primero se debe crear la vista materializada y luego ejecutar el siguiente código
-   
-   EXPLAIN PLAN FOR 
-   SELECT * FROM MV_AEROPUERTOS_AVIONES_JOVENES;
-
-   SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY());
-   
-   */
+*/
