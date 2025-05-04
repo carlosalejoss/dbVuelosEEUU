20bc7a3d4127163299ee8244181b8c77308eba60
carlosalejoss <872342@unizar.es>
2025-05-04 11:53:02 +0200

    Rename trigger to ensure clarity in airport diversion validation

--- sql/trigger3.sql
+++ sql/trigger3.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TRIGGER trg_validate_desvio_aeropuerto
+CREATE OR REPLACE TRIGGER trg_aeropuerto_desvio_distinto_destino
 BEFORE INSERT OR UPDATE ON DESVIO
 FOR EACH ROW
 DECLARE
@@ -11,10 +11,10 @@ BEGIN
     JOIN INCIDENCIA i ON v.idVuelo = i.idVuelo
     WHERE i.idIncidencia = :NEW.idIncidencia;
     
-    -- Comprobar si el aeropuerto de desvío es el mismo que el de llegada original
+    -- Comprobar si el aeropuerto de desvio es el mismo que el de llegada original
     IF :NEW.aeropuertoDesvio = v_aeropuerto_llegada THEN
         RAISE_APPLICATION_ERROR(-20201, 'El aeropuerto de desvio (' || :NEW.aeropuertoDesvio || 
-                              ') no puede ser el mismo que el aeropuerto de llegada original del vuelo.');
+                                        ') no puede ser el mismo que el aeropuerto de llegada original del vuelo.');
     END IF;
 END;
 /
